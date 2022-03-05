f89532891a5f45d2a1357cb68e06db9948683ea3
Kritarth D Kambli <kritarthkambli@gmail.com>
2022-03-05 20:14:45 +0530

    fixed error

--- GlassSum.java
+++ GlassSum.java
@@ -21,7 +21,7 @@ class HourglassSum {
     int sum = 0;
         for(int i = 0; i<arr.size()-2;i++){
             sum = 0;
-            for(int j = 0; j<arr.get(i).size()-2;i++){
+            for(int j = 0; j<arr.get(i).size()-2;j++){
                 sum  = sum +  arr.get(i).get(j) + arr.get(i).get(j+1) + arr.get(i).get(j+2) +arr.get(i+1).get(j+1)+arr.get(i+2).get(j)+arr.get(i+2).get(j+1)+arr.get(i+2).get(j+2);
             }
             if(max>sum){
